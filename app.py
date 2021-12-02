a21961ced9a1c21e65e941151c533b927bcdf0e6
qiikaii <sharpnshiny22@gmail.com>
2021-12-02 17:36:42 +0800

    csrf test

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 from flask import Flask, render_template, request, redirect, url_for, session
+from flask_wtf.csrf import CSRFProtect
+
 app = Flask(__name__)
+csrf = CSRFProtect(app)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
